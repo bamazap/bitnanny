a6078795c7a8ad3f918c55657ecce63df079d545
Barry Anthony McNamara III <bamazap@hotmail.com>
2018-04-19 20:34:28 -0400

    Fixed linter errors

--- src/app/analytics/analytics.component.ts
+++ src/app/analytics/analytics.component.ts
@@ -14,50 +14,67 @@ export class AnalyticsComponent implements OnInit {
   currentChild = 'Brian';
   currentActivity = 'Sleep';
   currentMetric = 'Mood';
-  
-  data = {'Brian': 
-	    {'Sleep': 
-	       {'Mood': 
-	          {x: [7, 3, 8, 5, 10, 9, 8, 4, 5, 6], y: [3, 1, 4, 3, 4, 3.5, 4.2, 1.2, 0.4, 2.1]}
-		}
-	     }
-         };
 
-  activityToLabel = {'Sleep': 'Hours of Sleep'};
-  metricToLabel = {'Mood': 'Mood'};
-  
+  data = {
+    Brian: {
+      Sleep: {
+        Mood: {
+          x: [7, 3, 8, 5, 10, 9, 8, 4, 5, 6],
+          y: [3, 1, 4, 3, 4, 3.5, 4.2, 1.2, 0.4, 2.1]
+        },
+      },
+    },
+  };
+
+  activityToLabel = {Sleep: 'Hours of Sleep'};
+  metricToLabel = {Mood: 'Mood'};
+
 
 
   getPlotData(child, activity, metric) {
-  	var currentX = this.data[child][activity][metric].x;
-  	var currentY = this.data[child][activity][metric].y;
-  	var trace: any = {
-  		x: currentX,
-  		y: currentY,
-  		mode: 'markers',
-  		type: 'scatter',
-  		marker: {size: 16, color: 'purple'}
-  	};
-  	var data = [trace];
-  	return data;
+    const currentX = this.data[child][activity][metric].x;
+    const currentY = this.data[child][activity][metric].y;
+    const trace: any = {
+      x: currentX,
+      y: currentY,
+      mode: 'markers',
+      type: 'scatter',
+      marker: {size: 16, color: 'purple'}
+    };
+    const data = [trace];
+    return data;
   }
 
   getPlotLayout(child, activity, metric) {
-  	var layout = {
-		title: activity + ' vs. ' + metric + ' for ' + child,
-		titlefont: { size: 18, color: 'purple'},
-		xaxis: {title: this.activityToLabel[activity], titlefont: {size: 16, color: 'purple'}},
-		yaxis: {title: this.metricToLabel[metric], titlefont: {size: 16, color: 'purple'}},	
-	};
-	return layout;
+    const layout = {
+    title: activity + ' vs. ' + metric + ' for ' + child,
+    titlefont: { size: 18, color: 'purple'},
+    xaxis: {
+      title: this.activityToLabel[activity],
+      titlefont: {size: 16, color: 'purple'}
+    },
+    yaxis: {
+      title: this.metricToLabel[metric],
+      titlefont: {size: 16, color: 'purple'}
+    },
+  };
+  return layout;
   }
 
   constructor() { }
 
   ngOnInit() {
-  	var data = this.getPlotData(this.currentChild, this.currentActivity, this.currentMetric);
-  	var layout = this.getPlotLayout(this.currentChild, this.currentActivity, this.currentMetric);
-  	Plotly.newPlot('myDiv', data, layout);
+    const data = this.getPlotData(
+      this.currentChild,
+      this.currentActivity,
+      this.currentMetric
+    );
+    const layout = this.getPlotLayout(
+      this.currentChild,
+      this.currentActivity,
+      this.currentMetric
+    );
+    Plotly.newPlot('myDiv', data, layout);
   }
 
 }
